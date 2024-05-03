85be3654f03b7ccc4740ea6c57a50119d09f65de
Nicholas Lima <95785812+Nicholas-Liman@users.noreply.github.com>
2024-05-03 15:06:46 -0300

    Add files via upload

---
+++ SQL_Create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE detalhes_login (email VARCHAR(200) CONSTRAINT clie_pk PRIMARY KEY,
+                            senha VARCHAR(200) CONSTRAINT senha_nn NOT NULL);
+
---
+++ SQL_Drop.sql
@@ -0,0 +1,1 @@
+DROP TABLE detalhes_login CASCADE CONSTRAINTS;
