4925b1e92726839b6555da99a6a439023fb844ba
Nicholas Lima <95785812+Nicholas-Liman@users.noreply.github.com>
2024-05-03 15:11:47 -0300

    Update SQL_Create.sql

--- SQL_Create.sql
+++ SQL_Create.sql
@@ -1,3 +1,1 @@
-CREATE TABLE detalhes_login (email VARCHAR(200) CONSTRAINT clie_pk PRIMARY KEY,
-                            senha VARCHAR(200) CONSTRAINT senha_nn NOT NULL);
-
+CREATE TABLE detalhes_login (email VARCHAR(200) CONSTRAINT clie_pk PRIMARY KEY, senha VARCHAR(200) CONSTRAINT senha_nn NOT NULL);
